0869bf17a91b6ac34043a1a41aa7c981cf3f68fd
Nashj <nashj@umich.edu>
2013-11-11 20:22:39 -0500

    Updated the timmy script to output with a 16k sampling rate, mono audio, and also removed the zip file because the wav file outputs are so small now. I am considering 8k sampling rates like the SVM paper, but it just seems like we would be throwing away higher harmonic information we could be using in classification

--- Music/timmy.sh
+++ Music/timmy.sh
@@ -3,6 +3,15 @@
 # ./timmy song.mid
 # outputs song.wav (keeps the original name)
 
+# Note on the 16k sampling rate:
+# The SVM paper uses an 8k sampling rate, but
+# I noticed a significant quality drop at 8k, 
+# whereas 16k seems to preserve the original quality
+# Note, too, that the highest key on the piano is 4186Hz,
+# which means 8k is insufficient, but this note also contains 
+# harmonics, so I think 16k is a good sampling rate to capture
+# these overtones.
+
 output=${1%.*}".wav"
-timidity $1 -OwM -o $output
+timidity $1 -OwM -o $output -s 16k
 
