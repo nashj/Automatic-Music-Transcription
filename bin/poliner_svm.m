c208d83e9a127f394ab208ebc7ab633ee331547a
Mark Liu <markmliu@umich.edu>
2013-12-11 03:32:06 -0500

    should handle multiple training inputs now

--- bin/poliner_svm.m
+++ bin/poliner_svm.m
@@ -1,4 +1,4 @@
-function [estimated_pr, pr] = poliner_svm(wav_train, midi_train, wav_test, midi_test)
+function [estimated_pr, pr] = poliner_svm()
   % Warning: this is written for Octave and not tested with MATLAB
   % This also uses libsvm. The libsvm I have included has been built for Octave
 
@@ -16,58 +16,88 @@ function [estimated_pr, pr] = poliner_svm(wav_train, midi_train, wav_test, midi_
   % Testing steps
   % 4. Run SVM on new wave file
   % 5. Run HMM on SVM piano roll output 
-  piano_low = 21;
-  piano_high = 107;
-  more off;
-
-  % Open wav file
-  [y,fs,bps] = wavread(wav_train);
-  [y_2,fs_2,bps_2] = wavread(wav_test);
-  % length(y)
-  % fs
-  % .128*fs
-  %.01*fs
-  % Compute the STFT on 128ms frames with 10ms hops
-  isOctave = exist('OCTAVE_VERSION') ~= 0;
-  if isOctave
-    [S, f, spec_t] = specgram(y, 2048, fs, hanning(.128*fs), 2048 - 160); % Needs to be spectrogram in MATLAB  
-  else
-    [S, f, spec_t] = spectrogram(y, 2048, 2048-160, 2048, fs);
-    [S_2, f_2, spec_t_2] = spectrogram(y_2, 2048, 2048-160, 2048, fs_2);
-
-    %[S, f, spec_t] = qgram(y, fs,1); 
-  end
-
-  size(S) 
-  %spec_t(end)
-  % pause
-  % Compute the FFT magnitudes
-  magS = abs(S);
-  magS_2 = abs(S_2);
-  % Normalize, whiten the FFT magnitudes (to-do)
-    
+  
   % Add the libsvm and midi libraries  
   addpath('../lib/libsvm');
   addpath('../lib/matlab-midi/src');
   
-
-  % Open the MIDI file
-  midi = readmidi(midi_train);
-  midi_2 = readmidi(midi_test);
-  notes = midiInfo(midi,0);
-  notes_2 = midiInfo(midi_2,0);
-  %notes = [notes; notes_2];
-  % T is in increments of 10ms, the same as our STFT'd wav file
-  [pr, t, nn] = piano_roll(notes);
-  [pr_2,t_2,nn_2]= piano_roll(notes_2);
-  
+  piano_low = 21;
+  piano_high = 107;
   nn_total = linspace(piano_low,piano_high,87);
-  %widen piano rolls to go from 21 to 107
-  pr = [pr;zeros(nn(1)-piano_low,size(pr,2))];
-  pr = [pr;zeros(piano_high-nn(end),size(pr,2))];
-  
-  pr_2 = [pr_2;zeros(nn(1)-piano_low,size(pr_2,2))];
-  pr_2 = [pr_2;zeros(piano_high-nn(end),size(pr_2,2))];
+  isOctave = exist('OCTAVE_VERSION') ~= 0;
+
+  train_wavs = dir('../data/train/*.wav');
+  train_mids = dir('../data/train/*.mid');
+  % number of training midi's should match number of training wav's...
+  test_wavs=  dir('../data/test/*.wav');
+  test_mids = dir('../data/test/*.mid');
+  more off;
+  Spec_trains = cell(size(train_wavs,1),4);
+  Spec_tests =  cell(size(test_wavs,1),4);
+  pr_trains = cell(size(train_wavs,1),2);
+  pr_tests = cell(size(test_wavs,1),2);
+  % Open wav file
+  notes_long = [];
+  for i=1:size(train_wavs,1)
+    train_name_w = train_wavs(i).name;
+    [y,fs,bps] = wavread(strcat('../data/train/',train_name_w));
+    if isOctave
+      [S, f, spec_t] = specgram(y, 2048, fs, hanning(.128*fs), 2048 - 160); % Needs to be spectrogram in MATLAB  
+    else
+      [S, f, spec_t] = spectrogram(y, 2048, 2048-160, 2048, fs);
+    end
+    magS = abs(S);
+    Spec_trains{i,1}=S;
+    Spec_trains{i,2}=f;
+    Spec_trains{i,3}=spec_t;
+    Spec_trains{i,4}=magS;
+    %Also grab the associated midi file...
+    prefix = strsplit(train_name_w,'.');
+    prefix = prefix{1};
+    train_name_m = strcat('../data/train/',prefix,'.mid');
+    midi = readmidi(train_name_m);
+    notes = midiInfo(midi,0);
+    if isempty(notes_long)
+        notes_long = notes;
+    else
+        notes_long = [notes_long; notes];
+    end
+    [pr, t, nn] = piano_roll(notes);
+    %widen piano rolls to go from 21 to 107
+    pr = [zeros(nn(1)-piano_low,size(pr,2));pr];
+    pr = [pr;zeros(piano_high-nn(end),size(pr,2))]; 
+    pr_trains{i,1} = pr;
+    pr_trains{i,2} = t;
+  end  
+    
+  for i=1:size(test_wavs,1)
+    test_name_w = test_wavs(i).name;
+    [y,fs,bps] = wavread(strcat('../data/test/',test_name_w));
+    if isOctave
+      [S, f, spec_t] = specgram(y, 2048, fs, hanning(.128*fs), 2048 - 160); % Needs to be spectrogram in MATLAB  
+    else
+      [S, f, spec_t] = spectrogram(y, 2048, 2048-160, 2048, fs);
+    end
+    magS = abs(S);
+    Spec_tests{i,1}=S;
+    Spec_tests{i,2}=f;
+    Spec_tests{i,3}=spec_t;
+    Spec_tests{i,4}=magS;
+    %Also grab the associated midi file...
+    prefix = strsplit(test_name_w,'.');
+    prefix = prefix{1};
+    test_name_m = strcat('../data/test/',prefix,'.mid');    
+    midi = readmidi(test_name_m);
+    notes = midiInfo(midi,0);
+    [pr, t, nn] = piano_roll(notes);
+    %widen piano rolls to go from 21 to 107
+    pr = [zeros(nn(1)-piano_low,size(pr,2));pr];
+    pr = [pr;zeros(piano_high-nn(end),size(pr,2))]; 
+    pr_tests{i,1} = pr;
+    pr_tests{i,2} = t;
+    
+  end    
+   
   %notes
   pause
   
@@ -77,8 +107,8 @@ function [estimated_pr, pr] = poliner_svm(wav_train, midi_train, wav_test, midi_
   % sum(pr,2)
 
   % Only display a subset of the ground truth for comparison to computed piano roll later
-  subset = 2000;
-  view_piano_roll(t_2(1:subset),nn_total,pr_2(:,1:subset), 'Ground truth')
+  subset = 1000;
+  view_piano_roll(pr_tests{1,2}(1:subset),nn_total,pr_tests{1,1}(:,1:subset), 'Ground truth')
   %view_piano_roll(spec_t(1:subset), nn, estimated_pr(:,1:subset));
 
   % This is a way to convert midi to wav, but it wasn't working for me:
@@ -91,10 +121,21 @@ function [estimated_pr, pr] = poliner_svm(wav_train, midi_train, wav_test, midi_
   svm_models = cell(num_notes); 
   means = cell(num_notes,1);
   vars = cell(num_notes,1);
+  %pr_long will be all the training pr's appended together  
+  %magS_long will be all the mag_S's appended together
+  pr_long = pr_trains{1,1};
+  magS_long = Spec_trains{1,4};
+  for i=2:size(pr_trains,1)
+      pr_long = [pr_long pr_trains{i,1}];    
+      magS_long = [magS_long Spec_trains{i,4}];
+  end
+  
+  
   for i=1:num_notes
       %index of i'th piano note in nn
       fprintf('Training SVM %d...\n', i);
-      note_vec = pr(i,:);
+      
+      note_vec = pr_long(i,:);
       
       if sum(note_vec) == 0
       	 continue
@@ -107,18 +148,18 @@ function [estimated_pr, pr] = poliner_svm(wav_train, midi_train, wav_test, midi_
       if length(pos) < 100
       	 num_examples = length(pos);
       else
-	 num_examples = 100;
+	     num_examples = 100;
          pos = pos(randperm(length(pos)));
       end
 
       pos = pos(1:num_examples);
-      pos_examples = magS(:,pos);
+      pos_examples = magS_long(:,pos);
 
       % Randomly sample same number of negative examples
       neg = find(note_vec == 0);
       neg = neg(randperm(length(neg)));
       neg = neg(1:num_examples);
-      neg_examples = magS(:,neg);
+      neg_examples = magS_long(:,neg);
       
       midi_note = nn_total(i);
       
@@ -145,10 +186,10 @@ function [estimated_pr, pr] = poliner_svm(wav_train, midi_train, wav_test, midi_
 
   % Just to test this for now, rerun these svms on the wave file and generate a piano roll
   % estimated_pr = zeros(size(pr));
-  estimated_pr = zeros(num_notes, size(S_2,2));
+  estimated_pr = zeros(num_notes, size(Spec_tests{1,4},2));
   estimated_pr = estimated_pr-1;
   % pr time length should equal S time length, but they're off by a few hundred samples, so I'm suspicious that timidity is adding extra time somewhere
-  training_pr = zeros(num_notes,size(S,2));
+  training_pr = zeros(num_notes,subset);
   training_pr = training_pr-1;
 
   for i=1:subset % size(S,2)
@@ -160,11 +201,11 @@ function [estimated_pr, pr] = poliner_svm(wav_train, midi_train, wav_test, midi_
         end
         midi_note = nn_total(j);
         if (midi_note >= 21 && midi_note <= 83) %0-2K
-           feature = magS_2(1:256, i);
+           feature = Spec_tests{1,4}(1:256, i);
         elseif (midi_note > 83 && midi_note <= 95) %1K-3K
-           feature = magS_2(129:384, i);          
+           feature = Spec_tests{1,4}(129:384, i);          
         else                                          %2K-4K
-           feature = magS_2(257:512, i);
+           feature = Spec_tests{1,4}(257:512, i);
         end        
         
 
@@ -178,11 +219,11 @@ function [estimated_pr, pr] = poliner_svm(wav_train, midi_train, wav_test, midi_
         %also evaluate svm on training wav file to get training_pr
         midi_note_train = nn_total(j);
         if (midi_note_train >= 21 && midi_note_train <= 83) %0-2K
-           feature = magS(1:256, i);
+           feature = magS_long(1:256, i);
         elseif (midi_note_train > 83 && midi_note_train <= 95) %1K-3K
-           feature = magS(129:384, i);          
+           feature = magS_long(129:384, i);          
         else                                          %2K-4K
-           feature = magS(257:512, i);
+           feature = magS_long(257:512, i);
         end         
         feature = feature - means{j}';
         feature = feature./vars{j}'; 
@@ -192,8 +233,9 @@ function [estimated_pr, pr] = poliner_svm(wav_train, midi_train, wav_test, midi_
       end
   end
   
-  view_piano_roll(spec_t_2(1:subset), nn_total, estimated_pr(:,1:subset), 'SVM output');
-  view_piano_roll(spec_t(1:subset), nn_total, training_pr(:,1:subset), 'training SVM output');
+  view_piano_roll(Spec_tests{1,3}(1:subset), nn_total, estimated_pr(:,1:subset), 'SVM output');
+  %if subset gets big, i'll have to do something else
+  view_piano_roll(Spec_trains{1,3}(1:subset), nn_total, training_pr(:,1:subset), 'training SVM output');
   
   fprintf('Running HMM on SVM output\n');
 
@@ -203,15 +245,15 @@ function [estimated_pr, pr] = poliner_svm(wav_train, midi_train, wav_test, midi_
   % Estimation of priors and transition matrix can be done using MIDI files (ground truth)
   % Estimation of emission matrix should be done using predict_label from SVM
   
-  notes_list = unique(notes(:,3)); %list of notes that are played on training data
+  notes_list = unique(notes_long(:,3)); %list of notes that are played on training data
   true_labels = zeros(size(training_pr));
   smooth_labels = zeros(size(training_pr));
   smooth_labels = smooth_labels-1;
   true_labels = bsxfun(@minus,true_labels,1); %initialize true_labels to -1
 
-  for i=1:size(nn,2)
+  for i=1:size(nn_total,2)
     fprintf('Smoothing note %d\n', i);
-    cur_note = nn(i);
+    cur_note = nn_total(i);
 
   % If cur_note is not actually played in song, skip it
     if (isempty(find(notes_list == cur_note, 1)))
@@ -219,7 +261,7 @@ function [estimated_pr, pr] = poliner_svm(wav_train, midi_train, wav_test, midi_
     end
 
   % Estimate prior
-    on_times = notes(notes(:,3) == cur_note,:);
+    on_times = notes_long(notes_long(:,3) == cur_note,:);
     on_time_total = 0;
     time_total = t(end); %using this as proxy for total length of time
     for j = 1:size(on_times,1)
@@ -279,7 +321,7 @@ function [estimated_pr, pr] = poliner_svm(wav_train, midi_train, wav_test, midi_
         continue;
     end
     %SEQ must be 1's and 2's.
-    SEQ = estimated_pr(i,1:2000);  
+    SEQ = estimated_pr(i,1:subset);  
     SEQ(SEQ==1)=2;
     SEQ(SEQ==-1)=1;
     trans_mat
@@ -291,9 +333,9 @@ function [estimated_pr, pr] = poliner_svm(wav_train, midi_train, wav_test, midi_
     smooth_labels(i,1:subset)= STATES;
     
   end
-  view_piano_roll(spec_t_2(1:subset), nn_2, smooth_labels(:,1:subset), 'Smoothed output');  
+  view_piano_roll(Spec_tests{1,3}(1:subset), nn_total, smooth_labels(:,1:subset), 'Smoothed output');  
   %Calculate error for raw and smoothed labels
-  [AccS, E_totS, E_subS, E_missS, E_faS] = calc_error(pr_2,smooth_labels, 2000);
-  [AccR, E_totR, E_subR, E_missR, E_faR] = calc_error(pr_2,estimated_pr, 2000);
+  [AccS, E_totS, E_subS, E_missS, E_faS] = calc_error(pr_trains{1,1},smooth_labels, subset);
+  [AccR, E_totR, E_subR, E_missR, E_faR] = calc_error(pr_trains{1,1},estimated_pr, subset);
 end
 
