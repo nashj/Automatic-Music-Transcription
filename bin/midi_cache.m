c62375d561f7265bcaea104817ea4b0cc1ea2104
Nashj <nashj@umich.edu>
2013-12-10 19:48:01 -0500

    Added a midi cache too, for the truly impatient

---
+++ bin/midi_cache.m
@@ -0,0 +1,15 @@
+function [pr, nn] = midi_cache(midi_file)
+  more off;
+  mat_cache = strcat(midi_file, '.cache');
+  try 
+      load(mat_cache);
+      fprintf('Cached MIDI found!\n');
+  catch		      
+      fprintf('Cached MIDI does not exist. Creating new cache.\n'); 		     
+      addpath('../lib/matlab-midi/src');
+      midi = readmidi(midi_file);
+      notes = midiInfo(midi,0);
+      [pr, g, nn] = piano_roll(notes);
+      save(mat_cache, 'pr', 'nn');
+  end
+end
